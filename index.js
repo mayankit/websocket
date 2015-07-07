53bc015e2de72502bb37d30a45047d132b8cc8cc
unknown <mayank.kumar@del1-dhp-27558.synapse.com>
2015-07-07 12:54:06 +0530

    added new file for websockets example.

---
+++ index.js
@@ -0,0 +1,25 @@
+var ws = new WebSocket("ws://127.0.0.1:8080/");
+
+ws.onopen = function() {
+    alert("Opened!");
+    ws.send("Hello Server");
+};
+
+ws.onmessage = function (evt) {
+    alert("Message: " + evt.data);
+};
+
+ws.onclose = function() {
+    alert("Closed!");
+};
+
+ws.onerror = function(err) {
+    alert("Error: " + err);
+};
+
+
+
+function sendMessage(){
+
+ws.send("abc");
+}
